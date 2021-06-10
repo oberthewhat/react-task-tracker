9ab9c14f6ff36c8784f431756ae484beeb0acfa3
John Obertubbesing <oberthewhat@gmail.com>
2021-06-10 12:20:28 -0500

    UI is complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Header from './components/Header' 
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 import { useState } from 'react'
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -25,6 +27,13 @@ function App() {
     },
 ])
 
+//add task
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000) + 1
+  const newTask = {id,...task}
+  setTasks([...tasks, newTask])
+}
+
 //delete task
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id))
@@ -32,12 +41,18 @@ const deleteTask = (id) => {
 
 //set reminder
 const toggleReminder = (id) => {
-  console.log(id)
+  setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
 }
 
   return (
     <div className="container">
-     <Header />
+     <Header 
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+          />
+     {showAddTask && <AddTask 
+        onAdd={addTask}
+      />}
      {tasks.length > 0 ? <Tasks 
      tasks={tasks} 
      onDelete={deleteTask}
